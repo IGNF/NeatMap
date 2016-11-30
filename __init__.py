f4603e7ee83d9605217fa92a37584b9801a04f3e
Julien Perret <julien.perret@gmail.com>
2016-11-30 15:16:43 +0100

    initial empty version from plugin builder

---
+++ __init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ TidyCity
+                                 A QGIS plugin
+ A simple QGIS python plugin for building tidy cities.
+                             -------------------
+        begin                : 2016-11-30
+        copyright            : (C) 2016 by IGN
+        email                : julien.perret@gmail.com
+        git sha              : $Format:%H$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load TidyCity class from file TidyCity.
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    #
+    from .tidy_city import TidyCity
+    return TidyCity(iface)
