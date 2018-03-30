46256a32b3b432790a7b27c7d50d5056930e2f6e
Mickaël Brasebin <mickael.brasebin@ign.fr>
2018-03-30 17:56:39 +0200

    Code to clasify feature with k-means and accessible from application code

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
+#!/usr/bin/env python3
+
 import sys
 import os
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
 from qgis.core import *
 from PyQt5.QtCore import QVariant
-from indicatorCalculation import *
+from TidyCity.indicatorCalculation import *
+from TidyCity.classification import *
+
 
 input_dir = "/home/mbrasebin/Documents/Donnees/Test/StageJimmy/"
 output_dir = "/home/mbrasebin/Documents/Donnees/temp/"
+
+fid_atribute = "fid"
+layerName = "bob"
 
 # supply path to qgis install location
 QgsApplication.setPrefixPath("/usr", True)
@@ -15,17 +27,35 @@ qgs = QgsApplication([], False)
 
 # load providers
 qgs.initQgis()
-print(qgs.showSettings())
 
 layer_polygons = QgsVectorLayer(os.path.join(input_dir,'extract.shp'), 'polygons', 'ogr')
 
+#Step 1 : calculating indicator
+layerOut = calculate(layerName,layer_polygons,fid_atribute);
+#Determining the attribute to use for the classification
+attributes = ["area", "elongation" , "compact."]
+#Applying the classification
+layerClassified = kmeans(layerOut, attributes, 3, layerName, "class", fid_atribute)
 
-layerOut = calculate("bob",layer_polygons,"fid");
 
-error = QgsVectorFileWriter.writeAsVectorFormat(layerOut, os.path.join(output_dir,"my_shapes.shp"),"utf-8", layerOut.crs(), "GeoJSON")
+error = QgsVectorFileWriter.writeAsVectorFormat(layerOut, os.path.join(output_dir,"indicator.shp"),"utf-8", layerOut.crs(), "ESRI Shapefile")
+
 
 if error == QgsVectorFileWriter.NoError:
     print("success!")
+else:
+    print(error)
+
+
+
+error = QgsVectorFileWriter.writeAsVectorFormat(layerClassified, os.path.join(output_dir,"classified.shp"),"utf-8", layerClassified.crs(), "ESRI Shapefile")
+
+
+if error == QgsVectorFileWriter.NoError:
+    print("success!")
+else:
+    print(error)
+
 
 
 qgs.exitQgis()
